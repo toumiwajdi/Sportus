48567cf4c68cf98824b2c2dee5b9af923946e953
Toumi Wajdi <toumi.wajdiat@gmail.com>
2017-01-23 11:26:50 +0100

    Add files via upload

---
+++ Sportus/Model/Classes/Serie.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 22/01/2017
+ * Time: 22:30
+ */
+class Serie
+{
+    private $id;
+    private $nom;
+    private $dateAjout;
+    private $description;
+    private $personne;
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateAjout()
+    {
+        return $this->dateAjout;
+    }
+
+    /**
+     * @param mixed $dateAjout
+     */
+    public function setDateAjout($dateAjout)
+    {
+        $this->dateAjout = $dateAjout;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPersonne()
+    {
+        return $this->personne;
+    }
+
+    /**
+     * @param mixed $personne
+     */
+    public function setPersonne($personne)
+    {
+        $this->personne = $personne;
+    }
+
+    function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return "{Serie : Id =".$this->getId().",Name =".$this->getNom().",Added Date =".$this->getDateAjout().",Description =".$this->getDescription().",Id Person =".$this->getPersonne() ."}";
+    }
+
+
+}
